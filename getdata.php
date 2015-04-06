fe738307446f82f34d8bd4107b00c9c01ccbb83c
Will Green <WilliamAnthonygreen@Gmail.com>
2015-04-06 20:51:26 +0100

    Final release
    Multiple updates. AJAX updates and JQuery replaces JavaScript. Pushed to Azure.
    
    Signed-off-by: Will Green <WilliamAnthonygreen@Gmail.com>

---
+++ getdata.php
@@ -0,0 +1,59 @@
+<?php
+
+			$bname = $_REQUEST["bname"];
+			$business = $_GET['bus'];
+
+        	$link = mysqli_connect('localhost', 'root', 'cabbage123'); 
+
+			$servername = "localhost";
+			$username = "root";
+			$password = "cabbage123";
+			$dbname = "success";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+    			die("Connection failed: " . $conn->connect_error);
+			}
+			// PHP for execution
+
+			$sql = "SELECT id, bname, bicon, rafrica, rasia, roceania, reurope, rsouthamerica, rnorthamerica, traffic, revenue, profit FROM business WHERE id = ".$business;
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0) {
+    			// output data of each row
+    			while($row = $result->fetch_assoc()) {
+        			$b3name = $row["bname"] ;
+        			$b3icon = $row["bicon"] ;
+        			$b3rafrica = $row["rafrica"] ;
+        			$b3rasia = $row["rasia"] ;
+        			$b3roceania = $row["roceania"] ;
+        			$b3reurope = $row["reurope"] ;
+        			$b3rsouthamerica = $row["rsouthamerica"] ;
+        			$b3rnorthamerica = $row["rnorthamerica"] ;
+        			$b3traffic = $row["traffic"] ;
+        			$b3revenue = $row["revenue"] ;
+        			$b3profit = $row["profit"] ;
+    			}
+			} else {
+    			echo "0 results";
+			}
+
+			$output = array(
+			    'name' => $b3name,
+			    'icon' => $b3icon,
+			    'rafrica' => $b3rafrica,
+			    'rasia' => $b3rasia,
+			    'roceania' => $b3roceania,
+			    'reurope' => $b3reurope,
+			    'rsouthamerica' => $b3rsouthamerica,
+			    'rnorthamerica' => $b3rnorthamerica,
+			    'traffic' => $b3traffic,
+			    'revenue' => $b3revenue,
+			    'profit' => $b3profit,
+			);
+			
+			echo json_encode($output);
+
+?>
